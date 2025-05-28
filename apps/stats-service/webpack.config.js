3f3616ebb0c0c542b467c6549b172dd21fd17c51
Ziad Alnajjar <ziadalnagdev@gmail.com>
2025-05-28 22:58:31 +0300

    build: add Docker, Makefile, Prisma, and update Nx targets
    - Add Dockerfiles and docker-compose setup
    - Create Makefiles
    - Update Nx targets
    - Add Prisma
    - Update project dependencies

--- apps/stats-service/webpack.config.js
+++ apps/stats-service/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/stats-service'),
+    path: join(__dirname, 'dist'),
   },
   plugins: [
     new NxAppWebpackPlugin({
